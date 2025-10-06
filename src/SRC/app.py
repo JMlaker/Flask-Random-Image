fea65231d0a6a374db1fb14f0c9a3442758785dd
Josh Mlaker <jmlaker@wisc.edu>
2025-10-06 12:32:29 -0500

    Added a config file (mostly for dev purposes)

--- src/SRC/app.py
+++ src/SRC/app.py
@@ -2,66 +2,9 @@ from flask import Flask, render_template, send_file, abort, jsonify, url_for, re
 import os
 import shutil
 import re
-import sys
-import random
-import numpy
-from itertools import repeat
 import ast
 
-def getRandom(dirs):
-    global imageWindow
-
-    weights = []
-    dirSum = 0
-    for dir in dirs:
-        weights.append(len(next(os.walk(dir))[2]))
-        dirSum += len(next(os.walk(dir))[2])
-    weights = list(map(lambda x: x / dirSum, weights))
-
-    if 0.0 in weights:
-        for i in range(len(weights)):
-            if weights[i] == 0.0:
-                del weights[i]
-                del dirs[i]
-
-    weights = [numpy.log10(x*1/(min(weights)**2)) for x in weights] if len(weights) > 1 else weights
-    weights = [x/sum(weights) for x in weights]
-    
-    randDir = random.choices(dirs, weights=weights, k=1)[0]
-    photoArr = next(os.walk(randDir))[2]
-    photoArr = [x for x in photoArr if "VID" not in x]
-    randImage = random.choice(photoArr)
-    imgPath = os.path.join(randDir, randImage)
-    return imgPath
-
-path = re.sub(f"{os.sep}SRC{os.sep}app.py", "", __file__)
-
-def directoryMapper(x, img_path = path):
-    return f"{img_path}{os.sep}{x}"
-
-def allDirectories(dir = [], img_path = path):
-    global path
-
-    # These have to be in here, but feel free to add some more
-    remove_dirs = ['favourites', 'trash', 'SRC'];
-
-    for x in next(os.walk(img_path))[1]:
-        dir.append(x)
-
-    for x in remove_dirs:
-        try:
-            dir.remove(x)
-        except:
-            pass
-
-    dir = list(map(directoryMapper, dir, repeat(img_path)))
-
-    t = []
-    for i in dir:
-        if len(os.listdir(i)) > 0:
-            t.append(i)
-
-    return t
+from utils import allDirectories, getRandom, directoryMapper, path, config
 
 app = Flask(__name__)
 # For dev purposes, this isn't randomized (this isn't my personal one either)
@@ -73,7 +16,6 @@ directories = allDirectories()
 def index():
     selected_dirs = session.get('selected_directories', directories)
     timer = session.get('timer', 2500)
-    favourite = session.get('favourite', 0)
 
     img = re.sub(f"src{os.sep}", "", os.path.relpath(getRandom(selected_dirs)))
     image_name = re.sub(f"..{os.sep}..{os.sep}", "", img)
@@ -196,6 +138,6 @@ def get_favourites():
 
 # By default, start it on localhost:5000, but feel free to change it
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host=config.get("FLASK-RUN", "host", fallback='127.0.0.1'), port=config.get("FLASK-RUN", "port", fallback='5000'), debug=True)
 
 
---
+++ src/SRC/utils.py
@@ -0,0 +1,70 @@
+import random
+import os
+import numpy
+import re
+from itertools import repeat
+from configparser import ConfigParser
+
+path = re.sub(f"{os.sep}SRC{os.sep}utils.py", "", __file__)
+
+config = ConfigParser()
+if os.path.exists(re.sub(f"src", "app.dev.ini", path)):
+    config.read(re.sub(f"src", "app.dev.ini", path))
+else:
+  config.read(re.sub(f"src", "app.ini", path))
+
+def getRandom(dirs):
+    global imageWindow
+
+    weights = []
+    dirSum = 0
+    for dir in dirs:
+        weights.append(len(next(os.walk(dir))[2]))
+        dirSum += len(next(os.walk(dir))[2])
+    weights = list(map(lambda x: x / dirSum, weights))
+
+    if 0.0 in weights:
+        for i in range(len(weights)):
+            if weights[i] == 0.0:
+                del weights[i]
+                del dirs[i]
+
+    weights = [numpy.log10(x*1/(min(weights)**2)) for x in weights] if len(weights) > 1 else weights
+    weights = [x/sum(weights) for x in weights]
+    
+    randDir = random.choices(dirs, weights=weights, k=1)[0]
+    photoArr = next(os.walk(randDir))[2]
+    photoArr = [x for x in photoArr if "VID" not in x]
+    randImage = random.choice(photoArr)
+    imgPath = os.path.join(randDir, randImage)
+    return imgPath
+
+def directoryMapper(x, img_path = path):
+    return f"{img_path}{os.sep}{x}"
+
+def allDirectories(dir = [], img_path = path):
+    global path
+
+    # These have to be in here, but feel free to add some more
+    remove_dirs = ['favourites', 'trash', 'SRC'];
+    
+    for i in [item.strip() for item in config.get("EXCLUDED-DIRS", "list", fallback=[]).split(", ")]:
+        remove_dirs.append(i)
+
+    for x in next(os.walk(img_path))[1]:
+        dir.append(x)
+
+    for x in remove_dirs:
+        try:
+            dir.remove(x)
+        except:
+            pass
+
+    dir = list(map(directoryMapper, dir, repeat(img_path)))
+
+    t = []
+    for i in dir:
+        if len(os.listdir(i)) > 0:
+            t.append(i)
+
+    return t
